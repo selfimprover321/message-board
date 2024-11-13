732200ce9bba05eec749e5fbd67a872374d1343b
selfimprover321 <136753822+selfimprover321@users.noreply.github.com>
2024-11-13 15:01:18 -0500

    added basic styling

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ const {indexRouter} = require('./routes/indexRouter')
 const newRouter = require('./routes/newRouter')
 const path = require('node:path')
 require('dotenv').config();
-
 const app = express();
 
 const port = process.env.PORT || 3000;
@@ -16,6 +15,7 @@ app.set('view engine', 'ejs')
 app.use('/', indexRouter);
 app.use('/new', newRouter);
 
+app.use(express.static(path.join(__dirname, "public")))
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`)
